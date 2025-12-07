a66200031cbb0bdc39d818b9f2741407a90711a2
Harsh Parmar <harshrparmar483@gmail.com>
2025-12-07 12:25:33 +0530

    Create CryptoResponse.java

---
+++ src/main/java/com/example/aesservice/dto/CryptoResponse.java
@@ -0,0 +1,19 @@
+package com.example.aesservice.dto;
+
+public class CryptoResponse {
+    private String result;
+
+    public CryptoResponse() {}
+
+    public CryptoResponse(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
