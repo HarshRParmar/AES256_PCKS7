0c7a06d5d70ff5bda3f6a7a97511d157dccc01d0
Harsh Parmar <harshrparmar483@gmail.com>
2025-12-07 12:19:52 +0530

    Create AesServiceApplication.java

---
+++ src/main/java/com/examnple/aesservice/AesServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.aesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AesServiceApplication.class, args);
+    }
+}
